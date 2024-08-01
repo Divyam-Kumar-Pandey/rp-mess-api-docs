ff9731b11d02e42a52fc77fd395ef44bd3860777
Divyam <divyamkumarp@gmail.com>
2024-08-02 03:37:22 +0530

    api docs

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,41 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+export default {
+    logo: "RP Mess Backend APIs",
+    project: {
+      link: 'https://github.com/Divyam-Kumar-Pandey/rp-mess-website-backend'
+    },
+    head: (
+        <>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta property="og:title" content="Divyam Kumar Pandey" />
+          <meta property="og:description" content="Documentation for RP Mess App APIs" />
+        </>
+    ),
+    useNextSeoProps() {
+      return {
+        titleTemplate: '%s | Divyam Kumar Pandey',
+      }
+    },
+    footer: {
+        text: (
+          <span>
+            2024 ©{' '}
+            <a href="https://github.com/Divyam-Kumar-Pandey" target="_blank">
+              Divyam Kumar Pandey
+            </a>
+            .
+          </span>
+        )
+      },
+    editLink: {
+        component: null
+    },
+    feedback: {
+        content: null
+    },
+    toc: {
+        float: true,
+        title: 'On this page',
+        backToTop: true,
 
-const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
-}
-
-export default config
+    }
+  }
